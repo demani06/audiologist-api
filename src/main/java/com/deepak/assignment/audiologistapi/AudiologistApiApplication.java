34960292a2f0c6b7be8179e034b425cb6c0f4ed7
Deepak Emani <deepakemani@gmail.com>
2018-11-14 20:09:01 +0000

    basic structure

---
+++ src/main/java/com/deepak/assignment/audiologistapi/AudiologistApiApplication.java
@@ -0,0 +1,12 @@
+package com.deepak.assignment.audiologistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudiologistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudiologistApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/deepak/assignment/audiologistapi/AudiologistApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.deepak.assignment.audiologistapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AudiologistApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
